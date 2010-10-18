79210ffad4115504f81fd2b8a9fb62485cd508c4
Caolan McMahon <caolan@caolanmcmahon.com>
2010-10-18 12:49:25 +0100

    added post processor support

--- test/test-wmd.js
+++ test/test-wmd.js
@@ -27,13 +27,35 @@ exports['preprocess'] = function (test) {
     test.done();
 };
 
+exports['postprocess'] = function (test) {
+    test.expect(3);
+    var options = {
+        postprocessors: [
+            function (doc) {
+                test.equals(doc, 'markdown');
+                return 'postprocessor 1';
+            },
+            function (doc) {
+                test.equals(doc, 'postprocessor 1');
+                return 'postprocessor 2';
+            }
+        ]
+    };
+    test.equals(
+        wmd.postprocess('markdown', options),
+        'postprocessor 2'
+    );
+    test.done();
+};
+
 exports['wmd'] = function (test) {
-    test.expect(8);
+    test.expect(10);
 
     // create a copy of all exported functions so we can safely stub them
     var _readOptions = wmd.readOptions;
     var _preprocess = wmd.preprocess;
     var _processor = wmd.processor;
+    var _postprocess = wmd.postprocess;
 
     wmd.readOptions = function (options) {
         test.equals(options, 'options');
@@ -49,10 +71,20 @@ exports['wmd'] = function (test) {
         test.equals(content, 'preprocessed');
         return 'processed';
     };
+    wmd.postprocess = function (content, options) {
+        test.same(content, {
+            markdown: 'preprocessed',
+            raw: 'content',
+            html: 'processed'
+        });
+        test.equals(options, 'read options');
+        content.html = 'postprocessed';
+        return content;
+    };
 
     var doc = wmd('content', 'options');
-    test.equals(doc, 'processed');
-    test.equals(doc.html, 'processed');
+    test.equals(doc, 'postprocessed');
+    test.equals(doc.html, 'postprocessed');
     test.equals(doc.raw, 'content');
     test.equals(doc.markdown, 'preprocessed');
 
@@ -60,6 +92,7 @@ exports['wmd'] = function (test) {
     wmd.readOptions = _readOptions;
     wmd.preprocess = _preprocess;
     wmd.processor = _processor;
+    wmd.postprocess = _postprocess;
     test.done();
 };
 
