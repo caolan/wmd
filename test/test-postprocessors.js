c292aa1a309038d73a0c150e19eb04ce5bec2f22
Caolan McMahon <caolan@caolanmcmahon.com>
2010-11-13 12:45:42 +0000

    fix use of first_para and heading postprocessors without jsdom already loaded

--- test/test-postprocessors.js
+++ test/test-postprocessors.js
@@ -4,13 +4,16 @@ var wmd = require('../lib/wmd'),
     heading = wmd.postprocessors.heading;
 
 
-var doc = {
-    html: '<h1>title text</h1>' +
-          '<p>first paragraph</p>' +
-          '<p>second paragrapg</p>'
-};
+function createDoc() {
+    return {
+        html: '<h1>title text</h1>' +
+              '<p>first paragraph</p>' +
+              '<p>second paragrapg</p>'
+    };
+}
 
 exports['jsdom'] = function (test) {
+    var doc = createDoc();
     var doc2 = jsdom(doc);
     test.equal(doc2.html, doc.html);
     test.equal(doc2.window.document.innerHTML, doc.html);
@@ -22,12 +25,14 @@ exports['jsdom'] = function (test) {
 };
 
 exports['first_para'] = function (test) {
+    var doc = createDoc();
     var doc2 = first_para(doc);
     test.equal(doc2.first_para, 'first paragraph');
     test.done();
 };
 
 exports['heading'] = function (test) {
+    var doc = createDoc();
     var doc2 = heading(doc);
     test.equal(doc2.heading, 'title text');
     test.done();
