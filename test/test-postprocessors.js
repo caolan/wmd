cc4878d118d6af34a59e3d64784132c85fba4a5d
Caolan McMahon <caolan@caolanmcmahon.com>
2010-11-13 12:50:07 +0000

    fix heading and first_para postprocessors when no p or h1 tags exist

--- test/test-postprocessors.js
+++ test/test-postprocessors.js
@@ -31,9 +31,23 @@ exports['first_para'] = function (test) {
     test.done();
 };
 
+exports['first_para without p tags'] = function (test) {
+    var doc = {html: '<h1>test</h1>'};
+    var doc2 = first_para(doc);
+    test.equal(doc2.first_para, null);
+    test.done();
+};
+
 exports['heading'] = function (test) {
     var doc = createDoc();
     var doc2 = heading(doc);
     test.equal(doc2.heading, 'title text');
+    test.done();
+};
+
+exports['heading without h1 tag'] = function (test) {
+    var doc = {html: '<p>test</p>'};
+    var doc2 = heading(doc);
+    test.equal(doc2.heading, null);
     test.done();
 };
